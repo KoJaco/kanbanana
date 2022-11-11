9b6fcf7e26bb2697287a556eab6bf3d9d524bc44
KoJaco <kojaco.dev@gmail.com>
2022-11-11 11:03:55 +1100

    chore: add resetDBForm, multi-stage form to be completed

---
+++ src/components/forms/ResetDBForm.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { resetDatabase } from '@/server/db';
+
+const ResetDBForm = () => {
+    // multi-stage form for resetting database.
+    return <div>ResetDBForm</div>;
+};
+
+export default ResetDBForm;
