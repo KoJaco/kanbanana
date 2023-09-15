7d585eba6a962269ef6849805dbbf45e4b958d2b
KoJaco <kojaco.dev@gmail.com>
2023-09-15 11:25:07 +1000

    feat: get everything ready to deploy.. coming back to real old project

--- src/pages/_offline.tsx
+++ src/pages/_offline.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
 
 const Offline = () => {
-    return <div>_offline</div>;
+    return (
+        <div className="flex flex-col h-screen w-full items-center justify-center">
+            Get back online boyyyy
+        </div>
+    );
 };
 
 export default Offline;
